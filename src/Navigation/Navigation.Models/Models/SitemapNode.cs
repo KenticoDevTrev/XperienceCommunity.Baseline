fc579ec622dae27f591564b78e96bbd896fd2971
Trevor Fayas <tfayas@gmail.com>
2023-06-27 21:12:54 -0500

    Move logic of Sitemap to string to the class and also add support for multilingual alternative

--- src/Navigation/Navigation.Models/Models/SitemapNode.cs
+++ src/Navigation/Navigation.Models/Models/SitemapNode.cs
@@ -11,8 +11,60 @@
         public Maybe<DateTime> LastModificationDate { get; set; }
         public Maybe<ChangeFrequency> ChangeFrequency { get; set; }
         public Maybe<decimal> Priority { get; set; }
+        public Maybe<Dictionary<string, string>> OtherLanguageCodeToUrl { get; set; }
 
+        /// <summary>
+        /// Helper to get an array of Sitemap Nodes to a valid sitemap xml
+        /// </summary>
+        /// <param name="sitemapNodes"></param>
+        /// <returns></returns>
+        public static string GetSitemap(IEnumerable<SitemapNode> sitemapNodes)
+        {
+            return $"<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" xmlns:xhtml=\"http://www.w3.org/1999/xhtml\">{string.Join("\n", sitemapNodes.Select(x => x.SitemapNodeToXmlString()))}</urlset>";
+        }
+
+        public string SitemapNodeToXmlString()
+        {
+            string changeFreq = string.Empty;
+            if (this.ChangeFrequency.HasValue)
+            {
+                switch (this.ChangeFrequency.Value)
+                {
+                    case Navigation.Models.ChangeFrequency.Always:
+                        changeFreq = "always";
+                        break;
+                    case Navigation.Models.ChangeFrequency.Daily:
+                        changeFreq = "daily";
+                        break;
+                    case Navigation.Models.ChangeFrequency.Hourly:
+                        changeFreq = "hourly";
+                        break;
+                    case Navigation.Models.ChangeFrequency.Monthly:
+                        changeFreq = "monthly";
+                        break;
+                    case Navigation.Models.ChangeFrequency.Never:
+                        changeFreq = "never";
+                        break;
+                    case Navigation.Models.ChangeFrequency.Weekly:
+                        changeFreq = "weekly";
+                        break;
+                    case Navigation.Models.ChangeFrequency.Yearly:
+                        changeFreq = "yearly";
+                        break;
+                }
+            }
+
+            return string.Format("<url>{0}{1}{2}{3}{4}</url>",
+                $"<loc>{Url}</loc>",
+            LastModificationDate.HasValue ? $"<lastmod>{LastModificationDate.Value.ToString("yyyy-MM-ddTHH:mm:sszzz")}</lastmod>" : "",
+            !string.IsNullOrWhiteSpace(changeFreq) ? $"<changefreq>{changeFreq}</changefreq>" : "",
+            Priority.HasValue ? $"<priority>{Priority.Value}</priority>" : "",
+            OtherLanguageCodeToUrl.TryGetValue(out var otherLanguageCodeToUrl) ? string.Join("", otherLanguageCodeToUrl.Keys.Select(langCode => $"<xhtml:link rel=\"alternate\" hreflang=\"{langCode}\" href=\"{otherLanguageCodeToUrl[langCode]}\"/>")) : string.Empty
+            );
+        }
     }
+
+
 
     //
     // Summary:
--- src/Navigation/Navigation.RCL/Features/Sitemap/SiteMapController.cs
+++ src/Navigation/Navigation.RCL/Features/Sitemap/SiteMapController.cs
@@ -34,60 +34,8 @@ namespace Navigation.Features.Sitemap
             }
 
             // Now render manually, sadly the SimpleMVCSitemap disables output cache somehow
-            return Content(GetSitemapXml(nodes), "text/xml", Encoding.UTF8);
+            return Content(SitemapNode.GetSitemap(nodes), "text/xml", Encoding.UTF8);
         }
 
-        /// <summary>
-        /// Formats the Nodes into the proper XML
-        /// </summary>
-        /// <param name="Nodes"></param>
-        /// <returns></returns>
-        private static string GetSitemapXml(IEnumerable<SitemapNode> nodes)
-        {
-                return $"<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">{string.Join("\n", nodes.Select(x => SitemapNodeToXmlString(x)))}</urlset>";
-        }
-
-        /// <summary>
-        /// Formats the Node into the proper XML
-        /// </summary>
-        /// <param name="Node"></param>
-        /// <returns></returns>
-        private static string SitemapNodeToXmlString(SitemapNode node)
-        {
-            string changeFreq = string.Empty;
-            if (node.ChangeFrequency.HasValue) {
-                switch (node.ChangeFrequency.Value)
-                {
-                    case ChangeFrequency.Always:
-                        changeFreq = "always";
-                        break;
-                    case ChangeFrequency.Daily:
-                        changeFreq = "daily";
-                        break;
-                    case ChangeFrequency.Hourly:
-                        changeFreq = "hourly";
-                        break;
-                    case ChangeFrequency.Monthly:
-                        changeFreq = "monthly";
-                        break;
-                    case ChangeFrequency.Never:
-                        changeFreq = "never";
-                        break;
-                    case ChangeFrequency.Weekly:
-                        changeFreq = "weekly";
-                        break;
-                    case ChangeFrequency.Yearly:
-                        changeFreq = "yearly";
-                        break;
-                }
-            }
-
-            return string.Format("<url>{0}{1}{2}{3}</url>",
-                $"<loc>{node.Url}</loc>",
-                node.LastModificationDate.HasValue ? $"<lastmod>{node.LastModificationDate.Value.ToString("yyyy-MM-ddTHH:mm:sszzz")}</lastmod>" : "",
-                !string.IsNullOrWhiteSpace(changeFreq) ? $"<changefreq>{changeFreq}</changefreq>" : "",
-                node.Priority.HasValue ? $"<priority>{node.Priority.Value}</priority>" : ""
-                );
-        }
     }
 }
