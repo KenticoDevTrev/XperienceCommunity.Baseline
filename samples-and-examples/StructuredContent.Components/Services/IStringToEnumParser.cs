00fe1888d9738a5b63ccdfcf4dfd5871c241d89b
Trevor Fayas <tfayas@gmail.com>
2023-06-30 12:15:20 -0500

    Update nuget packages and add tag helper info

---
+++ samples-and-examples/StructuredContent.Components/Services/IStringToEnumParser.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Services
+{
+    public interface IStringToEnumParser
+    {
+        IconSource StringToIconSource(string value);
+        LinkTargetType StringToLinkTargetType(string value);
+        TextAlignment StringToTextAlignment(string value);
+        VisualItemType StringToVisualItemType(string value);
+    }
+}
