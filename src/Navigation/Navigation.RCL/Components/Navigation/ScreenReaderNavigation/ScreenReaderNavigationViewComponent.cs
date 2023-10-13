44bf2904bdeda577b475478c24c6d852029f5c4b
Trevor Fayas <trevor.fayas@movement.com>
2023-10-13 15:51:44 -0500

    Add shortcut query selector

--- src/Navigation/Navigation.RCL/Components/Navigation/ScreenReaderNavigation/ScreenReaderNavigationViewComponent.cs
+++ src/Navigation/Navigation.RCL/Components/Navigation/ScreenReaderNavigation/ScreenReaderNavigationViewComponent.cs
@@ -2,21 +2,23 @@
 {
     public class ScreenReaderNavigationViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke(IEnumerable<NavigationItem> navigationItems, string navigationId)
+        public IViewComponentResult Invoke(IEnumerable<NavigationItem> navigationItems, string navigationId, string? navShortcutSelector = null)
         {
-            return View("/Components/Navigation/ScreenReaderNavigation/ScreenReaderNavigation.cshtml", new ScreenReaderNavigationViewModel(navigationItems, navigationId));
+            return View("/Components/Navigation/ScreenReaderNavigation/ScreenReaderNavigation.cshtml", new ScreenReaderNavigationViewModel(navigationItems, navigationId, navShortcutSelector));
         }
     }
 
     public record ScreenReaderNavigationViewModel
     {
-        public ScreenReaderNavigationViewModel(IEnumerable<NavigationItem> navigationItems, string id)
+        public ScreenReaderNavigationViewModel(IEnumerable<NavigationItem> navigationItems, string id, string? navShortcutSelector = null)
         {
             NavigationItems = navigationItems;
             Id = id;
+            NavShortcutSelector = navShortcutSelector.AsMaybe();
         }
 
         public IEnumerable<NavigationItem> NavigationItems { get; set; }
         public string Id { get; set; }
+        public Maybe<string> NavShortcutSelector { get; set; }
     }
 }
