4a70b591f5b578f161bc07af8b532215ec46d32a
Trevor Fayas <trevor.fayas@movement.com>
2023-12-20 21:10:43 -0600

    New identity models started

--- src/Core/Core.Models/Models/DocumentIdentity.cs
+++ src/Core/Core.Models/Models/DocumentIdentity.cs
@@ -1,7 +1,6 @@
-﻿using MVCCaching;
-
-namespace Core.Models
+﻿namespace Core.Models
 {
+    [Obsolete("Use ContentCultureIdentity")]
     public record DocumentIdentity : ICacheKey
     {
         public Maybe<int> DocumentId { get; init; }
@@ -16,7 +15,44 @@ namespace Core.Models
                 nodeAliasPathKey = $"{value.Item1}{value.Item2.GetValueOrDefault(string.Empty)}{value.Item3.GetValueOrDefault(0)}";
             }
             return $"{DocumentId.GetValueOrDefault(0)}{nodeAliasPathKey}{DocumentGuid.GetValueOrDefault(System.Guid.Empty)}";
+        }
 
+        public override int GetHashCode()
+        {
+            return GetCacheKey().GetHashCode();
+        }
+
+        public ContentCultureIdentity ToContentCultureIdentity()
+        {
+            return new ContentCultureIdentity()
+            {
+                ContentCultureID = DocumentId,
+                PathAndMaybeCultureAndChannelId = NodeAliasPathAndMaybeCultureAndSiteId,
+                ContentCultureGuid = DocumentGuid
+            };
+        }
+    }
+
+    public record ContentCultureIdentity : ICacheKey
+    {
+        public Maybe<int> ContentCultureID { get; init; }
+        public Maybe<Tuple<int, Maybe<string>>> MaybeContentIDAndMaybeCulture { get; init; }
+        public Maybe<Tuple<string, Maybe<string>, Maybe<int>>> PathAndMaybeCultureAndChannelId { get; init; }
+        public Maybe<Guid> ContentCultureGuid { get; init; }
+
+        public string GetCacheKey()
+        {
+            var nodeAliasPathKey = "";
+            var contentKey = "";
+            if (PathAndMaybeCultureAndChannelId.TryGetValue(out var valuePath))
+            {
+                nodeAliasPathKey = $"{valuePath.Item1}{valuePath.Item2.GetValueOrDefault(string.Empty)}{valuePath.Item3.GetValueOrDefault(0)}";
+            }
+            if (MaybeContentIDAndMaybeCulture.TryGetValue(out var valueContent))
+            {
+                contentKey = $"{valueContent.Item1}{valueContent.Item2.GetValueOrDefault(string.Empty)}";
+            }
+            return $"{ContentCultureID.GetValueOrDefault(0)}{nodeAliasPathKey}{contentKey}{ContentCultureGuid.GetValueOrDefault(System.Guid.Empty)}";
         }
 
         public override int GetHashCode()
--- src/Core/Core.Models/Models/NodeIdentity.cs
+++ src/Core/Core.Models/Models/NodeIdentity.cs
@@ -1,5 +1,6 @@
 ﻿namespace Core.Models
 {
+    [Obsolete("Use ContentIdentity or TreeIdentity")]
     public record NodeIdentity : ICacheKey
     {
         public Maybe<int> NodeId { get; init; }
@@ -13,8 +14,76 @@
             {
                 nodeAliasPathKey = $"{value.Item1}{value.Item2.GetValueOrDefault(0)}";
             }
-            return $"{NodeId.GetValueOrDefault(0)}{nodeAliasPathKey}{NodeGuid.GetValueOrDefault(System.Guid.Empty)}";
+            return $"{NodeId.GetValueOrDefault(0)}{nodeAliasPathKey}{NodeGuid.GetValueOrDefault(Guid.Empty)}";
+        }
 
+        public override int GetHashCode()
+        {
+            return GetCacheKey().GetHashCode();
+        }
+
+        public TreeIdentity ToTreeIdentity()
+        {
+            return new TreeIdentity()
+            {
+                PageID = NodeId,
+                PathAndAndChannelId = NodeAliasPathAndSiteId,
+                PageGuid = NodeGuid
+            };
+        }
+
+        public ContentIdentity ToContentIdentity()
+        {
+            return new ContentIdentity()
+            {
+                ContentID = NodeId,
+                PathAndAndChannelId = NodeAliasPathAndSiteId,
+                ContentGuid = NodeGuid
+            };
+        }
+    }
+
+    /// <summary>
+    /// Used as a lookup for Tree Items specifically
+    /// </summary>
+    public record TreeIdentity : ICacheKey
+    {
+        public Maybe<int> PageID { get; init; }
+        public Maybe<Tuple<string, Maybe<int>>> PathAndAndChannelId { get; init; }
+        public Maybe<Guid> PageGuid { get; init; }
+
+        public string GetCacheKey()
+        {
+            var nodeAliasPathKey = "";
+            if (PathAndAndChannelId.TryGetValue(out var valuePath))
+            {
+                nodeAliasPathKey = $"{valuePath.Item1}{valuePath.Item2.GetValueOrDefault(0)}";
+            }
+
+            return $"{PageID.GetValueOrDefault(0)}{nodeAliasPathKey}{PageGuid.GetValueOrDefault(Guid.Empty)}";
+        }
+
+        public override int GetHashCode()
+        {
+            return GetCacheKey().GetHashCode();
+        }
+    }
+
+    public record ContentIdentity : ICacheKey
+    {
+        public Maybe<int> ContentID { get; init; }
+        public Maybe<Tuple<string, Maybe<int>>> PathAndAndChannelId { get; init; }
+        public Maybe<Guid> ContentGuid { get; init; }
+
+        public string GetCacheKey()
+        {
+            var nodeAliasPathKey = "";
+            if (PathAndAndChannelId.TryGetValue(out var valuePath))
+            {
+                nodeAliasPathKey = $"{valuePath.Item1}{valuePath.Item2.GetValueOrDefault(0)}";
+            }
+           
+            return $"{ContentID.GetValueOrDefault(0)}{nodeAliasPathKey}{ContentGuid.GetValueOrDefault(Guid.Empty)}";
         }
 
         public override int GetHashCode()
