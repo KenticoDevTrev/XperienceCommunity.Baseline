e010f7da4c148935b9259e3d04c0ed0e3745ef09
Trevor Fayas <tfayas@gmail.com>
2024-12-21 20:15:34 -0600

    One small fix

--- src/Search/Search.Library.KX13/ServiceCollectionBaselineSearchExtensions.cs
+++ src/Search/Search.Library.KX13/ServiceCollectionBaselineSearchExtensions.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection.Extensions;
-using Search.Library.Xperience.Lucene.Services;
-using Search.Library.Xperience.Lucene.Services.Implementations;
-using Search.Repositories;
+﻿using Search.Repositories;
 using Search.Repositories.Implementations;
 
 namespace Microsoft.Extensions.DependencyInjection
@@ -9,11 +6,7 @@ namespace Microsoft.Extensions.DependencyInjection
     public static class ServiceCollectionBaselineSearchExtensions
     {
         /// <summary>
-        /// Adds the Baseline Search.  Note that for the ISearchRepository, it only implements a dummy version that will throw a not implemented exception.  You must choose a provider, then implement and inject your own version of the ISearchRepository
-        /// 
-        /// https://github.com/Kentico/xperience-by-kentico-lucene (Available through XperienceCommunity.Baseline.Search.Library.Xperience.Lucene and XperienceCommunity.Baseline.Search.Admin.Xperience.Lucene)
-        /// https://github.com/Kentico/xperience-by-kentico-algolia
-        /// https://github.com/Kentico/xperience-by-kentico-azure-ai-search
+        /// Adds the Baseline Search (Lucene)
         /// </summary>
         /// <param name="services"></param>
         /// <returns></returns>
